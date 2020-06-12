9fd0bd0b899f0991888ede1591945fa95e7bbff3
Ren Waldura <ren@waldura.org>
2020-06-12 14:43:50 -0700

    compute per-min and per-10 min wind averages

--- update-db.sh
+++ update-db.sh
@@ -2,26 +2,50 @@
 # Every minute, update aggregates, and insert into db
 #
 
+WEATHER_DB=/var/weather/weather.db
+db=${1:-$WEATHER_DB}
+
 # where the wind data is continuously stored
 WIND_LOG=/var/weather/log/wind
-source=${1:-$WIND_LOG}
+source=${2:-$WIND_LOG}
 
-# we generate a sample every 3 seconds, or 20 per minute
-# look back 5 minutes, to make sure we don't miss any
+# a sample is collected every 3 seconds, or 20 per minute
+# look back the last 5 minutes, to make sure we don't miss any
 NUM_SAMPLES=100
 
 buffer=/tmp/$(basename $0).$$
 tail -$NUM_SAMPLES $source > $buffer
 
-sqlite3 <<_SQL_
+sqlite3 $db <<_SQL_
+CREATE TABLE IF NOT EXISTS wind_1min  (tstamp INTEGER UNIQUE, direction INTEGER, speed INTEGER);
+CREATE TABLE IF NOT EXISTS wind_10min (tstamp INTEGER UNIQUE, direction INTEGER, speed INTEGER);
+CREATE TABLE IF NOT EXISTS wind_1hr   (tstamp INTEGER UNIQUE, direction INTEGER, speed INTEGER);
+
 CREATE TEMP TABLE raw_wind_data (tstamp TIMESTAMP, direction INTEGER, speed INTEGER, revs INTEGER);
 .mode tabs
 .import $buffer raw_wind_data
-INSERT OR REPLACE INTO wind_minute 
+
+BEGIN;
+
+-- compute per-minute averages
+INSERT OR REPLACE INTO wind_1min 
 	SELECT 
-		strftime("%Y-%m-%d %H:%M", tstamp, "unixepoch", "localtime") as minute, 
-		AVG(speed) AS speed,
-		AVG(direction) AS direction
+		60 * (tstamp / 60), -- round to the lowest minute
+		ROUND(AVG(direction)),
+		AVG(speed)
 	FROM raw_wind_data
-	GROUP BY minute;
+	GROUP BY 1;
+	
+-- compute per-10 minute averages
+INSERT OR REPLACE INTO wind_10min 
+	SELECT 
+		60 * 10 * (tstamp / (60 * 10)), -- round to the lowest 10 minutes
+		ROUND(AVG(direction)),
+		AVG(speed)
+	FROM raw_wind_data
+	GROUP BY 1;
+	
+COMMIT;
 _SQL_
+
+# 	ON CONFLICT(tstamp) DO UPDATE SET direction = EXCLUDED.direction, speed = EXCLUDED.speed;
