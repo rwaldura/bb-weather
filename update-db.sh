b82f04e7f39ecd90681d627c38bae759a504cb0b
Ren Waldura <ren@waldura.org>
2020-06-13 10:25:39 -0700

    compute (and store) wind parameter aggregates for the last min, 3 min, 10 min, 1 hour

--- update-db.sh
+++ update-db.sh
@@ -10,43 +10,66 @@ db=${1:-$WEATHER_DB}
 
 # where the wind data is continuously stored
 WIND_LOG=/var/weather/log/wind
-source=${2:-$WIND_LOG}
+log=${2:-$WIND_LOG}
 
 # a sample is collected every 3 seconds, or 20 per minute
 # look back the last 10 minutes, to make sure we don't miss any
 NUM_SAMPLES=222
+n=${3:-$NUM_SAMPLES}
 
 buffer=/tmp/$(basename $0).$$
-tail -$NUM_SAMPLES $source > $buffer
+tail -$n $log > $buffer
 
 sqlite3 $db <<_SQL_
-CREATE TABLE IF NOT EXISTS wind_1min  (tstamp INTEGER UNIQUE, direction INTEGER, speed INTEGER);
-CREATE TABLE IF NOT EXISTS wind_10min (tstamp INTEGER UNIQUE, direction INTEGER, speed INTEGER);
-CREATE TABLE IF NOT EXISTS wind_1hr   (tstamp INTEGER UNIQUE, direction INTEGER, speed INTEGER);
+CREATE TABLE IF NOT EXISTS wind_1m  (tstamp INTEGER NOT NULL UNIQUE, direction INTEGER, speed INTEGER);
+CREATE TABLE IF NOT EXISTS wind_3m  (tstamp INTEGER NOT NULL UNIQUE, direction INTEGER, speed INTEGER);
+CREATE TABLE IF NOT EXISTS wind_10m (tstamp INTEGER NOT NULL UNIQUE, direction INTEGER, speed INTEGER);
+CREATE TABLE IF NOT EXISTS wind_1h  (tstamp INTEGER NOT NULL UNIQUE, direction INTEGER, speed INTEGER);
+-- CREATE UNIQUE INDEX IF NOT EXISTS tstamp_aggr ON wind(tstamp, aggr);
 
-CREATE TEMPORARY TABLE raw_wind_data (tstamp INTEGER, direction INTEGER, speed INTEGER, revs INTEGER);
+CREATE TEMPORARY TABLE wind_log (tstamp INTEGER, direction INTEGER, speed INTEGER, revs INTEGER);
 .mode tabs
-.import $buffer raw_wind_data
+.import $buffer wind_log
 
 BEGIN;
 
 -- compute per-minute averages
-INSERT OR REPLACE INTO wind_1min 
+INSERT OR REPLACE INTO wind_1m
 	SELECT 
-		60 * (tstamp / 60), -- round to the lowest minute
+		(1 * 60) * (tstamp / (1 * 60)), -- round to the lowest minute
 		ROUND(AVG(direction)),
 		AVG(speed)
-	FROM raw_wind_data
+	FROM wind_log
+	GROUP BY 1;
+
+-- compute per-3 minute averages
+INSERT OR REPLACE INTO wind_3m 
+	SELECT 
+		(3 * 60) * (tstamp / (3 * 60)), -- round to the lowest 3 minutes
+		ROUND(AVG(direction)),
+		AVG(speed)
+	FROM wind_1m
+	WHERE tstamp > (SELECT MIN(tstamp) FROM wind_log)
 	GROUP BY 1;
 	
 -- compute per-10 minute averages
-INSERT OR REPLACE INTO wind_10min 
+INSERT OR REPLACE INTO wind_10m 
 	SELECT 
 		(10 * 60) * (tstamp / (10 * 60)), -- round to the lowest 10 minutes
 		ROUND(AVG(direction)),
 		AVG(speed)
-	FROM wind_1min
-	WHERE tstamp > (SELECT MIN(tstamp) FROM raw_wind_data)
+	FROM wind_1m
+	WHERE tstamp > (SELECT MIN(tstamp) FROM wind_log)
+	GROUP BY 1;
+
+-- compute per-hour averages
+INSERT OR REPLACE INTO wind_1h
+	SELECT 
+		(60 * 60) * (tstamp / (60 * 60)), -- round to the lowest hour
+		ROUND(AVG(direction)),
+		AVG(speed)
+	FROM wind_1m
+	WHERE tstamp > (SELECT MIN(tstamp) FROM wind_log)
 	GROUP BY 1;
 	
 COMMIT;
