682325e525b41c20bd5a59aeddda7f24a07b9d16
Ren Waldura <ren@waldura.org>
2020-06-14 22:56:08 -0700

    generate test data based on current time -- rather than cycling through values

--- gen-test-wind-data.sh
+++ gen-test-wind-data.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-s=0 # speed
-d=0 # direction
-
 while true
 do
-	t=$( date +%s )
-	echo "$t\t$d\t$s\t0"
-	s=$(( ($s + 10) % 200 )) 
-	d=$(( ($d + 18) % 360 )) 
+	date +"%s	%H	%M	%S"
 	sleep 3
 done
 
