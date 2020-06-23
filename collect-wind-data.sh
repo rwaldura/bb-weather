603379c0c35c271fe2bb6d354e968548cec74f9d
Ren Waldura <ren@waldura.org>
2020-06-23 13:39:22 -0700

    cannot pump data into the main db; it becomes locked, and read-only. I.e. that was a bad idea

--- collect-wind-data.sh
+++ collect-wind-data.sh
@@ -9,8 +9,7 @@ WIND_LOG=/var/weather/log/wind
 # temporary database for ongoing wind data
 temp_dir=/tmp
 test -d "$XDG_RUNTIME_DIR" && temp_dir=$XDG_RUNTIME_DIR
-#WIND_DB=$temp_dir/wind.db
-WIND_DB=/var/weather/weather.db
+WIND_DB=$temp_dir/wind.db
 
 ROTATION_PERIOD=86400
 
