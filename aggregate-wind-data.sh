b9f37863889d31e09e5e0c05dbbe4bf5942d4e08
Ren Waldura <ren@waldura.org>
2020-08-17 21:26:32 -0700

    fixed bag bug in data aggregator: the last record was ignored

--- aggregate-wind-data.sh
+++ aggregate-wind-data.sh
@@ -4,9 +4,6 @@
 # Note we always *overwrite* any existing data. I.e. the input is source of
 # truth.
 #
-# In practice, we update (or create) the last aggregate only; i.e. the 
-# aggregate for the current time period. 
-#
 
 readonly SQLITE_FUNCS=/usr/lib/libsqlitefunctions.so
 test -r $SQLITE_FUNCS || {
@@ -17,9 +14,18 @@ test -r $SQLITE_FUNCS || {
 readonly WEATHER_DB=/var/weather/weather.db
 db=${1:-$WEATHER_DB}
 
+# Two distinct processes write to the database: this one, and
+# populate-wind-db.sh. This database may therefore get busy.
+# If a lock is encountered when trying to write, keep on re-trying for ~10 sec.
+# See https://www.sqlite.org/c3ref/busy_timeout.html
+readonly BUSY_TIMEOUT=11111
+
 date # log timestamp
 
 sqlite3 "$db" <<_SQL_
+-- deal with concurrency issues by re-trying
+PRAGMA busy_timeout = $BUSY_TIMEOUT;
+
 -- load extra functions like MEDIAN() etc.
 SELECT load_extension('$SQLITE_FUNCS');
 
@@ -34,7 +40,8 @@ INSERT OR REPLACE INTO wind
 		wind
 	WHERE
 		period = 1
-		AND tstamp >= (10 * 60) * (strftime('%s', 'now') / (10 * 60))  -- current timeperiod only
+		-- current and last timeperiod
+		AND tstamp >= (10 * 60) * (strftime('%s', 'now') / (10 * 60)) - (10 * 60)
 	GROUP BY 1;
 
 -- compute 1-hour aggregate
@@ -48,7 +55,8 @@ INSERT OR REPLACE INTO wind
 		wind
 	WHERE
 		period = 1		
-		AND tstamp >= (60 * 60) * (strftime('%s', 'now') / (60 * 60))  -- current timeperiod only
+		-- current and last timeperiod
+		AND tstamp >= (60 * 60) * (strftime('%s', 'now') / (60 * 60)) - (60 * 60)
 	GROUP BY 1;
 _SQL_
 
